c4a259952b85ff5cbb5770b9a50d526498e86cb4
Christian Müller <pub01@navalla.org>
2007-06-21 15:40:13 +0000

    Import of CHRMUE/PAB3-DB-Driver-Sqlite3-1.0.3 from CPAN.
    gitpan-cpan-distribution: PAB3-DB-Driver-Sqlite3
    gitpan-cpan-version:      1.0.3
    gitpan-cpan-path:         CHRMUE/PAB3-DB-Driver-Sqlite3-1.0.3.tar.gz
    gitpan-cpan-author:       CHRMUE
    gitpan-cpan-maturity:     released

--- Sqlite3.pm
+++ Sqlite3.pm
@@ -13,7 +13,7 @@ no warnings 'uninitialized';
 use vars qw($VERSION @EXPORT_FNC @EXPORT_CONST $MODPERL $SessionCleanup);
 
 BEGIN {
-	$VERSION = '1.0.2';
+	$VERSION = '1.0.3';
 	
 	require XSLoader;
 	XSLoader::load( __PACKAGE__, $VERSION );
